5cf9a5518bc1805fc5e47c0cb6d6b3f462bdeba2
Chong Lee <clee010922@Chongs-MacBookPro.local>
2022-08-01 01:09:58 -0400

    added API url constants, progress on first API call function

--- main.js
+++ main.js
@@ -1,8 +1,27 @@
+const apiData1 = {
+    url: "https://api.spoonacular.com/recipes/",
+    findBy: "findByIngredients?ingredients=",
+    ingredients: "",
+    apiKey: "&apiKey=2c95ce6156a248b08e61bcc20f220575"
+}
 
-/*
-function foo() {
-     let a = document.getElementById("input").value
-     console.log(a); 
-    }
-*/
+const apiData2 = {
+    url: "https://api.spoonacular.com/recipes/",
+    recipeID: "",
+    etc: "/information?includeNutrition=false",
+    apiKey: "&apiKey=2c95ce6156a248b08e61bcc20f220575"
+}
+
+function apiFirstCall() {
+     let ingr = document.getElementById("input").value;
+     apiData1.ingredients = String(ingr);
+     console.log(ingr); 
+
+     const {url, findBy, ingredients, apiKey} = apiData1;
+     const apiURL = `${url}${findBy}${ingredients}${apiKey}`;
+     apiData1.ingredients = "";
+     console.log(apiURL);
+     console.log(apiData1.ingredients);
+}
+
 
