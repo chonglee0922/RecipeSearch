f44ef09be97d2dca6603559ce20de1bf4feecf30
Chong Lee <clee010922@Chongs-MacBookPro.local>
2022-08-01 21:12:08 -0400

    completed first API call function

--- main.js
+++ main.js
@@ -3,16 +3,19 @@ const apiData1 = {
     findBy: "findByIngredients?ingredients=",
     ingredients: "",
     apiKey: "&apiKey=2c95ce6156a248b08e61bcc20f220575"
-}
+};
 
 const apiData2 = {
     url: "https://api.spoonacular.com/recipes/",
     recipeID: "",
     etc: "/information?includeNutrition=false",
     apiKey: "&apiKey=2c95ce6156a248b08e61bcc20f220575"
-}
+};
 
-function apiFirstCall() {
+const dish_list = document.querySelector(".dishList");
+const dish_info = document.querySelector(".dishInfo");
+
+function firstAPIcall() {
      let ingr = document.getElementById("input").value;
      apiData1.ingredients = String(ingr);
      console.log(ingr); 
@@ -22,6 +25,18 @@ function apiFirstCall() {
      apiData1.ingredients = "";
      console.log(apiURL);
      console.log(apiData1.ingredients);
+
+     fetch(apiURL)
+        .then(response => response.json())
+        .then((data1) => {
+            for (let i = 0; i < data1.length; i++) {
+                let newListElement = document.createElement("li");
+                let dishName = document.createElement("p");
+                dishName.setAttribute("target", "_blank");
+                //dishName.addEventListener("click", function() {secondAPIcall(data[i].id);});
+                dishName.textContent = data1[i].title;
+                newListElement.appendChild(dishName);
+                dish_list.appendChild(newListElement);
+            }
+        })
 }
-
-
